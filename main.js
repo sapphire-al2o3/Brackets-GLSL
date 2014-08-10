66b0c57b76810935d9d33c6f38f8bcc31e1569b5
tel <t_telkun@hotmail.com>
2014-08-10 12:17:49 +0900

    コメントのショートカットを動くようにした

--- main.js
+++ main.js
@@ -2,15 +2,19 @@
 define(function (require, exports, module) {
 	'use strict';
 	
-	var CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
+	var LanguageManager = brackets.getModule("language/LanguageManager"),
+		CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
 	
-	CodeMirror.defineMIME("text/x-brackets-html", {
-		"name": "htmlmixed",
-		"scriptTypes": [
-			{ "matches": /x-shader\/x-fragment/i, "mode": "x-shader/x-fragment" },
-			{ "matches": /x-shader\/x-vertex/i, "mode": "x-shader/x-vertex" }
-		]
+	CodeMirror.resolveMode("text/x-brackets-html").scriptTypes.push(
+		{ "matches": /x-shader\/x-fragment/i, "mode": "x-shader/x-fragment" },
+		{ "matches": /x-shader\/x-vertex/i, "mode": "x-shader/x-vertex" }
+	);
+	
+	LanguageManager.defineLanguage("clike", {
+		"name": "clike",
+		"mode": "clike",
+		"blockComment": ["/*", "*/"],
+		"lineComment": ["//"]
 	});
-	
 });
 
