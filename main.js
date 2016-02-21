2d42eb68c01578930b8596b4e2b08f4f80de5e59
tel <t_telkun@hotmail.com>
2016-02-21 14:57:06 +0900

    補完リストに定義を表示する

--- main.js
+++ main.js
@@ -41,13 +41,18 @@ define(function (require, exports, module) {
 	});
 
 	function add(list, words, token, filter) {
-		for(var i = 0, n = words.length; i < n; i++) {
-			if(!words[i].filter || words[i].filter === filter) {
-				if(words[i].name.indexOf(token) >= 0) {
+		for (var i = 0, n = words.length; i < n; i++) {
+			if (!words[i].filter || words[i].filter === filter) {
+				if (words[i].name.indexOf(token) >= 0) {
 					var $hint = $('<span>');
 					$hint.addClass('brackets-js-hints');
+					$hint.addClass('brackets-js-hints-with-type-details');
 					$hint.data(words[i].name);
-					list.push($hint.html(words[i].name.replace(token, '<span style="font-weight: 500;">' + token + '</span>')));
+					$hint.html(words[i].name.replace(token, '<span style="font-weight: 500;">' + token + '</span>'));
+					if (words[i].def) {
+						$('<span>' + words[i].def + '</span>').appendTo($hint).addClass('jshint-description');
+					}
+					list.push($hint);
 				}
 			}
 		}
@@ -56,8 +61,8 @@ define(function (require, exports, module) {
 	function GLSLHints() {
 	}
 	
-	GLSLHints.prototype.hasHints = function(editor, implicitChar) {
-		if(!implicitChar || /[\w#_]/.test(implicitChar)) {
+	GLSLHints.prototype.hasHints = function (editor, implicitChar) {
+		if (!implicitChar || /[\w#_]/.test(implicitChar)) {
 			var cursor = editor.getCursorPos(),
 				ctx = TokenUtils.getInitialContext(editor._codeMirror, cursor);
 			
@@ -71,7 +76,7 @@ define(function (require, exports, module) {
 				this.isFragment = ctx.token.state.localMode.helperType === 'x-shader/x-fragment';
 				this.pre = implicitChar === '#' || ctx.token.string[0] === '#';
 				
-				if(TokenUtils.movePrevToken(ctx)) {
+				if (TokenUtils.movePrevToken(ctx)) {
 					if (ctx.token.string[0] === '.') {
 						return false;
 					}
@@ -82,23 +87,23 @@ define(function (require, exports, module) {
 		return false;
 	};
 	
-	GLSLHints.prototype.getHints = function(implicitChar) {
+	GLSLHints.prototype.getHints = function (implicitChar) {
 		var hintList = [],
 			cursor = this.editor.getCursorPos(),
 			token = this.editor._codeMirror.getTokenAt(cursor);
 		
-		if(implicitChar === null && cursor.ch <= this.start) {
+		if (implicitChar === null && cursor.ch <= this.start) {
 			return null;
 		}
 		
-		if(' /()[],;.'.indexOf(implicitChar) >= 0) {
+		if (' /()[],;.'.indexOf(implicitChar) >= 0) {
 			return null;
 		}
 		
 		var str = token.string.slice(0, cursor.ch - token.start),
 			filter = this.isFragment ? "frag" : 'vert';
 		
-		if(this.pre) {
+		if (this.pre) {
 			add(hintList, glslWords.pres, str, filter);
 		} else {
 			add(hintList, glslWords.variables, str, filter);
@@ -115,7 +120,7 @@ define(function (require, exports, module) {
 		};
 	};
 	
-	GLSLHints.prototype.insertHint = function(hint) {
+	GLSLHints.prototype.insertHint = function (hint) {
 		var cursor = this.editor.getCursorPos(),
 			start = {line: cursor.line, ch: this.start},
 			end = cursor;
@@ -123,7 +128,7 @@ define(function (require, exports, module) {
 		return false;
 	};
 	
-	AppInit.appReady(function() {
+	AppInit.appReady(function () {
 		var glslHints = new GLSLHints();
 		CodeHintManager.registerHintProvider(glslHints, ['clike'], 0);
 	});
